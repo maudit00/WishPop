6b92f9bd5f4e0f15c28c88aa2829afd899bd5730
Maudit00 <davide9311@gmail.com>
2023-12-11 12:43:29 +0100

    environments done

--- wishpop/src/app/Services/auth.service.ts
+++ wishpop/src/app/Services/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
+import { ILogin } from '../Models/i-login';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +11,8 @@ export class AuthService {
   constructor(private http:HttpClient, private route:Router) {
   }
 
-login ()
+login(data:ILogin){
+
+}
 
 }
