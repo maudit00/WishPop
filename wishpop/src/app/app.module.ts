ab14d04d64abb7d9f825849b3566d49a0480bca3
Micol1234 <micollombardi@libero.it>
2023-12-11 18:30:25 +0100

    app-header

--- wishpop/src/app/app.module.ts
+++ wishpop/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from './header/header.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
