4c4aa9b51072748506db6f4bddb9f10c5ce8414d
Maudit00 <davide9311@gmail.com>
2023-12-11 14:45:10 +0100

    auth module imported

--- wishpop/src/app/app.module.ts
+++ wishpop/src/app/app.module.ts
@@ -1,15 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { AuthComponent } from './Services/auth/auth.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    AuthComponent
   ],
   imports: [
     BrowserModule,
