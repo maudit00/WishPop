fc3f5aa8fc9bafbfd3b6057c5c782cb032126344
andreacruciani9 <140162473+andreacruciani9@users.noreply.github.com>
2023-12-11 18:12:13 +0100

    home-page

--- wishpop/src/app/app-routing.module.ts
+++ wishpop/src/app/app-routing.module.ts
@@ -5,7 +5,8 @@ const routes: Routes = [
   {
     path: 'auth',
      loadChildren: () => import('./pages/auth/auth.module').then(m => m.AuthModule)
-  }];
+  },
+  { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- wishpop/src/app/pages/auth/auth-routing.module.ts
+++ wishpop/src/app/pages/auth/auth-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './auth.component';
 import { LoginComponent } from './login/login.component';
+import { HomeComponent } from '../../home/home.component';
 
 const routes: Routes = [
   { path: '', component: AuthComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent },
 ];
 
 @NgModule({
